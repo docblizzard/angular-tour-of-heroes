c9fbff37fc797661f9040e9032cfe9785bb1490c
BASSAGET Elwin <bassagete@ardian.com>
2022-09-24 18:33:11 +0200

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,8 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 12, name: 'Dr Who'},
+    {id: 13, name: 'Mr Fantastic'},
+    {id: 14, name: 'Thor'},
+    {id: 15, name: 'Magneto'}
+]
